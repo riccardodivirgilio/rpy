df582a274d6d466ef622acbe10551f34d7567fbe
Riccardo Di Virgilio <riccardod@wolfram.com>
2018-11-08 11:55:29 +0100

    adding delete tests

--- rpy/tests/test_keychain.py
+++ rpy/tests/test_keychain.py
@@ -2,27 +2,26 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
+import shutil
+import tempfile
 import unittest
 
+from rpy.functions.api import fernet
 from rpy.password.keychain import KeyChain
-from rpy.functions.api import json, fernet
 
-import tempfile
-import shutil
 
 class TestCase(unittest.TestCase):
-
     def credentials(self):
         return tempfile.mkdtemp(), fernet.Fernet.generate_key()
 
     def test_single_single(self):
 
         f1, p1 = self.credentials()
-        
+
         kc = KeyChain(f1, p1)
         kc.set_secret("foo", {"x": 2})
 
-        self.assertEqual(kc.get_secret("foo"), {"x":2})
+        self.assertEqual(kc.get_secret("foo"), {"x": 2})
 
         shutil.rmtree(f1)
 
@@ -31,26 +30,34 @@ class TestCase(unittest.TestCase):
         f1, p1 = self.credentials()
         f2, p2 = self.credentials()
         f3, p3 = self.credentials()
-        
+
         kc = KeyChain(
-            {'default': f1, 'sensitive': f2, 'developer': f3},
-            {'default': p1, 'sensitive': p2, 'developer': p3},
+            {
+                'default': f1,
+                'sensitive': f2,
+                'developer': f3
+            },
+            {
+                'default': p1,
+                'sensitive': p2,
+                'developer': p3
+            },
         )
 
-        
-        for secret_name in ('default', 'sensitive', 'developer', None): 
+        for secret_name in ('default', 'sensitive', 'developer', None):
 
             for value in ({"x": 2}, 4):
-                kc.set_secret("name", value, secret_name = secret_name)
+                kc.set_secret("name", value, secret_name=secret_name)
                 self.assertEqual(
-                    kc.get_secret("name", secret_name = secret_name), 
-                    value
-                )
+                    kc.get_secret("name", secret_name=secret_name), value)
 
             self.assertEqual(
-                tuple(kc.list_secrets(secret_name = secret_name)),
-                ('name', )
-            )
+                tuple(kc.list_secrets(secret_name=secret_name)), ('name', ))
+
+            kc.delete_secret('name', secret_name=secret_name)
+
+            self.assertEqual(
+                tuple(kc.list_secrets(secret_name=secret_name)), ())
 
         shutil.rmtree(f1)
         shutil.rmtree(f2)
