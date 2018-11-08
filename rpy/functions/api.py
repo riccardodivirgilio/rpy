7f6f150673ec1e3d62787aeccff86eedf8de4872
Riccardo Di Virgilio <riccardod@wolfram.com>
2018-11-08 11:55:51 +0100

    minor fixes

--- rpy/functions/api.py
+++ rpy/functions/api.py
@@ -2,9 +2,8 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-from rpy.functions.importutils import API
 from rpy.functions.encoding import force_bytes
-
+from rpy.functions.importutils import API
 
 pip = API(
     main=('pip.main', 'pip._internal.main'),
@@ -16,19 +15,17 @@ pip = API(
     running_under_virtualenv=(
         'pip.locations.running_under_virtualenv',
         'pip._internal.locations.running_under_virtualenv',
-    ))
+    )
+)
 
 base64 = API(
-    dumps='base64.b64encode', 
+    dumps='base64.b64encode',
     loads='base64.b64decode',
     urlsafe_dumps='base64.urlsafe_b64encode',
     urlsafe_loads='base64.urlsafe_b64decode'
 )
 
-json = API(
-    loads = 'json.loads', 
-    dumps = 'json.dumps'
-)
+json = API(loads='json.loads', dumps='json.dumps')
 
 fernet = API(
     Fernet = 'cryptography.fernet.Fernet',
