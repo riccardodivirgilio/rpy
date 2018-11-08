fd2479e3c34154106f99ecb782a93dd7dd2527a7
Riccardo Di Virgilio <riccardod@wolfram.com>
2018-11-08 16:44:14 +0100

    incrementing vers

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def load_tests():
 
 setup(
     name = 'python-rpy',
-    version = '1.0.0',
+    version = '1.0.1',
     description = 'A Python library with various functional tools.',
     long_description = read('README.rst'),
     long_description_content_type = 'text/x-rst',
