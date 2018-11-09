44e61e9b286a55c618af0e8959a1788898438940
Riccardo Di Virgilio <riccardod@wolfram.com>
2018-11-09 23:22:56 +0100

    using main

--- rpy/__main__.py
+++ rpy/__main__.py
@@ -2,8 +2,10 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-if __name__ == '__main__':
-
+def main():
     from rpy.cli.dispatch import execute_from_command_line
+    execute_from_command_line()    
 
-    execute_from_command_line()
+if __name__ == '__main__':
+    main()
+
