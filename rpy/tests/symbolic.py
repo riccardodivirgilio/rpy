8238ba4fe93b2709533806e4a75b8f377c258767
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-02-07 14:34:14 +0100

    replacements

--- rpy/tests/symbolic.py
+++ rpy/tests/symbolic.py
@@ -14,7 +14,7 @@ class SymbolicTest(unittest.TestCase):
         for expr, context, result in (
             (sym.x + 2,             {'x': 2}, 4),
             (sym.int(sym.x),        {'x': 3.2}, 3),
-            (sym.x + sym.z + sym.y, {'x': sym.y, 'y': 4, 'z': 2}, 10),
+            (sym.x + sym.z + sym.y, {'x': sym.y - 2, 'y': sym.z * 4, 'z': 2}, 16),
             (sym.x(2, 3, 4),        {'x': lambda a, b, c: a + b + c}, 9),
             (sym.x.join(['ciao', 'bella']), {'x': ', '}, 'ciao, bella'),
             (sym.x['foo'], {'x': {'foo': 2}}, 2),
