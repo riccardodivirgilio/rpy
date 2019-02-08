dee38ca18acddc72ad15236a3fcbb31051ccbbc3
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-02-08 13:29:11 +0100

    refactoring excel to support symbolic computations

--- rpy/dataframe/excel.py
+++ rpy/dataframe/excel.py
@@ -6,20 +6,27 @@ import datetime
 from decimal import Decimal
 from itertools import count, islice, product, repeat
 
+from rpy.dataframe.symbolic import Symbolic, evaluate
 from rpy.functions import six
 from rpy.functions.decorators import to_tuple
 from rpy.functions.dispatch import Dispatch
 from rpy.functions.encoding import force_text
+from rpy.functions.escape import escape
 
 XLSX_MYMETYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
 
 class Formula(object):
 
-    def __init__(self, formula):
-        self.formula = formula
+    def __init__(self, formula, cell_format = None, extimated_size = None):
+        self.formula     = formula
+        self.cell_format = cell_format
+        self.extimated_size = extimated_size
 
     def __repr__(self):
         return '<%s %s>' % (self.__class__.__name__, self.formula)
+
+    def __str__(self):
+        return '(%s)' % self.formula
 
 def _column_name_generator(alphabeth = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
     for i in count(1):
@@ -29,81 +36,140 @@ def _column_name_generator(alphabeth = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
 def get_excel_names(r):
     return islice(_column_name_generator(), r)
 
-write = Dispatch()
+def excel_enumerate(iterable):
+    return zip(count(), _column_name_generator(), iterable)
+
+class CallableText(object):
+
+    operators = {
+        'add': ' + ',
+        'sub': ' - ',
+        'mul': ' * ',
+        'pow': '^',
+        'truediv': ' / ',
+        'floordiv': ' / ',
+    }
+
+    functions = {
+        'floordiv': 'FLOOR(%s, 1)'
+    }
+
+    def __init__(self, symbol):
+        self.name = force_text(symbol)
+
+    def __call__(self, *args):
+
+        args = self.normalize_args(args)
+
+        try:
+            inner = self.operators[self.name].join(args)
+            templ = self.functions.get(self.name, None) or '(%s)'
+        except KeyError:
+            inner = ', '.join(args)
+            templ = self.functions.get(self.name, None) or (self.name.upper() + '(%s)')
+
+        return templ % inner
+
+    def normalize_args(self, args):
+        for arg in args:
+            if isinstance(arg, CallableText):
+                yield force_text(arg)
+            else:
+                arg = to_formula(arg)
+                if isinstance(arg, Formula):
+                    yield '(%s)' % arg.formula
+                elif arg:
+                    yield escape(arg)
+
+    def __str__(self):
+        return self.name
+
+to_formula = Dispatch()
+
+@to_formula.dispatch(Symbolic)
+def handle(value):
+    f = evaluate(
+        value, 
+        missing_function = CallableText, 
+        context = {}, 
+        default_context = {}
+    )
+    return Formula(f)
 
 
+@to_formula.dispatch(Formula)
+def handle(value):
+    return value
 
-@write.dispatch(Formula)
-def handle(self, value, y, x):
-    return self.worksheet.write_formula(y, x, value.formula)
+@to_formula.dispatch(bool)
+def handle(value):
+    return Formula(value and 'TRUE' or 'FALSE', extimated_size = value and 4 or 5)
 
-@write.dispatch(bool)
-def handle(self, value, y, x):
-    self.update_max_size(x, 4)
-    return self.worksheet.write_formula(y, x, (value and '=TRUE' or '=FALSE'))
+@to_formula.dispatch(datetime.timedelta)
+def handle(value):
+    return Formula('TIME(0, 0, 0) + (%s / 86400) - TIME(0, 0, 0)' % value.total_seconds())
 
-@write.dispatch((tuple, list, set, frozenset))
-def handle(self, value, y, x):
-    return write(self, "-".join([force_text(v) for v in value]), y, x)
-
-@write.dispatch(datetime.timedelta)
-def handle(self, value, y, x):
-    return self.worksheet.write_formula(y, x, '=TIME(0, 0, 0) + (%s / 86400) - TIME(0, 0, 0)' % value.total_seconds())
-
-@write.dispatch(datetime.datetime)
-def handle(self, value, y, x):
-    self.update_max_size(x, 18)
-    return self.worksheet.write_formula(y, x, '=DATE(%s, %s, %s) + (%s * 3600 + %s * 60 + %s) / 86400' % (
-        value.year,
-        value.month,
-        value.day,
-        value.hour,
-        value.minute,
-        value.second + value.microsecond / 1000000,
+@to_formula.dispatch(datetime.datetime)
+def handle(value):
+    return Formula(
+        'DATE(%s, %s, %s) + (%s * 3600 + %s * 60 + %s) / 86400' % (
+            value.year,
+            value.month,
+            value.day,
+            value.hour,
+            value.minute,
+            value.second + value.microsecond / 1000000,
         ),
-        cell_format = self.get_format('datetime_format', 'dd-mmm-yy hh:mm:ss')
+        cell_format = ('datetime_format', 'dd-mmm-yy hh:mm:ss'),
+        extimated_size = 18
     )
 
-@write.dispatch(datetime.date)
-def handle(self, value, y, x):
-    self.update_max_size(x, 9)
-    return self.worksheet.write_formula(y, x, '=DATE(%s, %s, %s)' % (value.year, value.month, value.day),
-        cell_format = self.get_format('date_format', 15)
-        )
+@to_formula.dispatch(datetime.date)
+def handle(value):
+    return Formula(
+        'DATE(%s, %s, %s)' % (value.year, value.month, value.day),
+        cell_format = ('date_format', 15),
+        extimated_size = 9
+    )
 
-@write.dispatch(datetime.time)
-def handle(self, value, y, x):
-    self.update_max_size(x, 8)
-    return self.worksheet.write_formula(y, x, '=TIME(%s, %s, %s) + (%s / 86400)' % (
-        value.hour,
-        value.minute,
-        value.second,
-        value.microsecond / 1000000,
+@to_formula.dispatch(datetime.time)
+def handle(value):
+    return Formula(
+        'TIME(%s, %s, %s) + (%s / 86400)' % (
+            value.hour,
+            value.minute,
+            value.second,
+            value.microsecond / 1000000,
         ),
-        cell_format = self.get_format('time_format', 21)
-        )
+        cell_format = ('time_format', 21),
+        extimated_size = 8
+    )
 
-@write.dispatch(int)
-def handle(self, value, y, x):
-    return self.worksheet.write_formula(y, x, '=%s' % value,
-        cell_format = self.get_format('int_format', 3) # #,##0
-        )
+@to_formula.dispatch(int)
+def handle(value):
+    return Formula(
+        '%s' % value,
+        cell_format = ('int_format', 3) # #,##0
+    )
 
-@write.dispatch((float, Decimal))
-def handle(self, value, y, x):
-    return self.worksheet.write_formula(y, x, '=%s' % value,
-        cell_format = self.get_format('dec_format', 4) # #,##0.00
-        )
+@to_formula.dispatch((float, Decimal))
+def handle(value):
+    return Formula(
+        '%s' % value,
+        cell_format = ('dec_format', 4) # #,##0.00
+    )
 
-@write.dispatch(type(None))
-def handle(self, value, y, x):
-    pass
+@to_formula.dispatch((tuple, list, set, frozenset))
+def handle(value):
+    return ",".join([force_text(v) for v in value])
 
-@write.dispatch()
-def handle(self, value, y, x):
-    value = force_text(value)
-    self.update_max_size(x, len(value))
-    return self.worksheet.write(y, x, value)
+@to_formula.dispatch(type(None))
+def handle(value):
+    return ""
+
+@to_formula.dispatch()
+def handle(value):
+    return force_text(value)
 
 class XLSXWriter(object):
 
@@ -164,7 +230,21 @@ class XLSXWriter(object):
 
         self.worksheet = None
 
-    write = write.as_method()
+    def write(self, value, y, x):
+        formula = to_formula(value)
+
+        #this can be string, or formula
+
+        if isinstance(formula, Formula):
+            if formula.extimated_size:
+                self.update_max_size(x, formula.extimated_size)
+            return self.worksheet.write_formula(
+                y, x, formula.formula, 
+                cell_format = formula.cell_format and self.get_format(*formula.cell_format) or None
+            )
+        elif formula:
+            self.update_max_size(x, len(formula))
+            return self.worksheet.write(y, x, formula)
 
     def writerow(self, row):
         for y, value in enumerate(row):
