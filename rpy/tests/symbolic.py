2acf1d976a4af3b540090921586d68010207875e
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-02-08 11:24:47 +0100

    adding more tests

--- rpy/tests/symbolic.py
+++ rpy/tests/symbolic.py
@@ -14,11 +14,13 @@ class SymbolicTest(unittest.TestCase):
         for expr, context, result in (
             (sym.x + 2,             {'x': 2}, 4),
             (sym.int(sym.x),        {'x': 3.2}, 3),
+            (sym.float(sym.x),      {'x': 3}, 3.),
             (sym.x + sym.z + sym.y, {'x': sym.y - 2, 'y': sym.z * 4, 'z': 2}, 16),
             (sym.x(2, 3, 4),        {'x': lambda a, b, c: a + b + c}, 9),
             (sym.x.join(['ciao', 'bella']), {'x': ', '}, 'ciao, bella'),
             (sym.x['foo'], {'x': {'foo': 2}}, 2),
             (sym.x[2:3], {'x': [1, 2, 3, 4, 5]}, [3]),
+            (sym.sum(sym.x), {'x': [1, 2, 3]}, 6),
             ):
             self.assertEqual(evaluate(expr), expr)
             self.assertEqual(evaluate(expr, context), result)
