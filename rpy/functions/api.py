03c8133ba1fb6d428f48743fb3261573fbdea3c7
Riccardo Di Virgilio <riccardod@wolfram.com>
2020-04-08 16:31:59 +0200

    fixing pip

--- rpy/functions/api.py
+++ rpy/functions/api.py
@@ -6,7 +6,7 @@ from rpy.functions.encoding import force_bytes
 from rpy.functions.importutils import API
 
 pip = API(
-    main=('pip.main', 'pip._internal.main'),
+    main=("pip.main", "pip._internal.main", "pip._internal.main.main"),
     get_installed_distributions=(
         'pip.get_installed_distributions',
         'pip.utils.get_installed_distributions',
