301ca720fe16636e95d5bc00903bbfc7f2efbc7d
Riccardo Di Virgilio <riccardod@wolfram.com>
2020-04-08 17:04:56 +0200

    removing symbolic step

--- rpy/dataframe/array.py
+++ rpy/dataframe/array.py
@@ -13,15 +13,15 @@ def render_value(formula, length, context):
     return evaluate(formula, context=context)
 
 @to_array.dispatch((six.integer_types, float, six.string_types))
-def render_value(value, length, context):
+def render_value(value, length, **opts):
     return np.full(length, value)
 
 @to_array.dispatch(dict)
-def render_value(d, length, context):
+def render_value(d, length, **opts):
 
     # same implementation of np.core.records.fromarrays
 
-    columns = tuple((k, to_array(v, length, context)) for k, v in d.items())
+    columns = tuple((k, to_array(v, length, **opts)) for k, v in d.items())
 
     a = np.recarray((length,), list((k, v.dtype) for k, v in columns))
 
@@ -31,14 +31,18 @@ def render_value(d, length, context):
     return a
 
 @to_array.dispatch((list, tuple, types.GeneratorType))
-def render_value(value, length, context):
-    return to_array({"f%i" % i: v for i, v in enumerate(value)}, length, context)
+def render_value(value, length, **opts):
+    return to_array({"f%i" % i: v for i, v in enumerate(value)}, length, **opts)
+
+@to_array.dispatch(np.ndarray)
+def render_value(value, length, **opts):
+    return value
 
 def array_map(func, array):
-    return to_array(func(sym.array), length=len(array), context = {'array': array})
+    return to_array(func(array), length=len(array))
 
 def array_map_indexed(func, array):
-    return to_array(func(sym.array, sym.index), length=len(array), context = {'array': array, 'index': np.arange(len(array))})
+    return to_array(func(array, np.arange(len(array))), length=len(array))
 
 def array_filter(func, array):
     return array[array_map(func, array)]
